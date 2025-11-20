e45d0b610ff69dc42565b099acdd1d00b3b98be8
Nehal Kadyan <nehalkadyan@Nehals-MacBook-Air.local>
2025-11-20 12:33:24 +0530

    done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Home = () => {
+  //
+
   return (
     <div>
       <h1>Home Page</h1>
-    </div>
-  )
-}
+      <h3>Update Username</h3>
 
-export default Home
+      <form>
+        <input
+          style={{ padding: 10, backgroundColor: "white", color: "black",
+            margin: 10, borderRadius: 10, outline: "none"
+           }}
+          type="text"
+          placeholder="Enter new username"
+        />
+        <br />
+        <button type="submit">Update Username</button>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
